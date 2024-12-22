a25088db451e439bc56af8766690b89ee13b2e74
DavidVLiberman <davidliberman17@gmail.com>
2024-12-22 01:22:14 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+// React frontend code for the user interface
+import React from 'react';
+function App() {
+    return (
+        <div>
+            <h1>AI Platform</h1>
+            <p>Welcome to the AI Platform!</p>
+        </div>
+    );
+}
+export default App;
